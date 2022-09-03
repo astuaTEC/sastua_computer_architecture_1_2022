27062a678af1a527aad8f9a74b28d317c3cc2f5f
Saymon <sam.astua7@gmail.com>
2022-09-02 22:42:15 -0600

    Primera version para leer archivos

--- proyecto_1/src/asm/test.asm
+++ proyecto_1/src/asm/test.asm
@@ -1,11 +1,63 @@
+section .data
+    msgError db "Se produjo un error",0xa,0xd
+    lon equ $-msgError
+
+    msgExito db "Archivo abierto con exito",0xa,0xd
+    lonExito equ $-msgExito
+
+    archivo db "archivo.txt",0
+
+section .bss
+idArchivo resd 1
+contenido resb 16384
+
 section .text
 global _start
+
 _start:
-mov rax, 3
-mov rbx, 7
-_presuma:
-add rax, rbx
-_suma:
-mov rax, 60
-mov rdi, 0
-syscall
+    mov eax, 5
+    mov ebx, archivo
+    mov ecx, 0002h ;lectura y escritura
+    int 0x80
+
+    cmp eax, 0
+    jl error
+
+    mov dword[idArchivo], eax
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msgExito
+    mov edx, lonExito
+    int 0x80
+
+    mov eax, 3
+    mov ebx, [idArchivo]
+    mov ecx, contenido
+    mov edx, 16384
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, contenido
+    mov edx, 16384
+    int 0x80
+
+    mov eax, 6
+    mov ebx, [idArchivo]
+    int 0x80
+
+    jmp salir
+
+error:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msgError
+    mov edx, lon
+    int 0x80
+
+salir:
+    mov eax,1
+    mov ebx,0
+    int 0x80
+
