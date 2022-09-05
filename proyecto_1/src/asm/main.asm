95f9e6a336542686b9dc6f35499d4d66f41e3984
Saymon <sam.astua7@gmail.com>
2022-09-04 19:31:48 -0600

    Se accede a los vertices para aplicar el algoritmo

--- proyecto_1/src/asm/main.asm
+++ proyecto_1/src/asm/main.asm
@@ -27,7 +27,27 @@ idArchivo resd 1
 contenido resb 50000 ; Almacenar n cantidad de bytes
 
 idArchivoDest resd 1
-contenidoDest resb 50000;
+contenidoDest resb 50000
+
+a resd 1
+b resd 1
+c resd 1
+d resd 1
+e resd 1
+f resd 1
+g resd 1
+h resd 1
+i resd 1
+j resd 1
+k resd 1
+
+v1 resd 1
+v2 resd 1
+v3 resd 1
+v4 resd 1
+
+row2 resd 1
+col2 resd 1
 
 section .text
 global _start
@@ -58,9 +78,74 @@ _start:
     mov edx, 50000 ; leer cierta cantidad de bytes
     int 0x80
 
-    mov esi, contenido; dirección de memoria de contenido
+    mov r8, 0 ;contador de filas
 
-    jmp atoi
+
+loopFilas:
+    mov r9, 0 ; se limpia el contador de columnas
+    cmp r8, 97 ; se compara el contador de filas
+    jb loopCol ; si es menor, se imprime una nueva columna    
+
+loopCol:
+
+getV1:
+    mov r10, contenido
+    imul r11, r8, 16 ; se obtiene la fila
+    imul r12, r9, 4 ; se obtiene la columna
+    add r11, r12 ; se suma fila + columna
+    add r10, r11 ; se suma a la posicion
+
+    mov rsi, r10
+    call atoi 
+    mov [v1], edi
+
+getV2:
+    mov r10, contenido
+    imul r11, r8, 16 ; se obtiene la fila
+    mov r12, r9
+    inc r12 ; col + 1
+    imul r12, 4 ; se obtiene la columna
+    add r11, r12 ; se suma fila + columna
+    add r10, r11 ; se suma a la posicion
+    
+    mov rsi, r10 
+    call atoi
+    mov [v2], edi
+    
+getV3:
+    mov r10, contenido
+    mov r11, r8
+    inc r11; row + 1
+    imul r11, 16 ; se obtiene la fila
+    imul r12, r8, 4 ; se obtiene la columna
+    add r11, r12 ; se suma fila + columna
+    add r10, r11 ; se suma a la posicion
+
+    mov rsi, r10 
+    call atoi
+
+    mov [v3], edi
+
+getV4:
+    mov r10, contenido
+    mov r11, r8
+    inc r11 ; row + 1
+    imul r11, 16 ; se obtiene la fila
+    mov r12, r9
+    inc r12 ; col + 1
+    imul r12, 4 ; se obtiene la columna
+    
+    add r11, r12 ; se suma fila + columna
+    add r10, r11 ; se suma a la posicion
+
+    mov rsi, r10 
+    call atoi
+
+    mov [v4], edi
+
+    mov r14d, [v4]
+
+    jmp done
 
 error:
     mov eax, 4
@@ -74,18 +159,20 @@ error:
 atoi:
     xor edi, edi ; zero a "result so far"
 top:
-    movzx ebp, byte [esi] ; get a character
-    inc esi ; ready for next one
+    movzx ebp, byte [rsi] ; get a character
+    inc rsi ; ready for next one
     cmp ebp, '0' ; valid?
-    jb done
+    jb done_m
     cmp ebp, '9'
-    ja done
+    ja done_m
     sub ebp, '0' ; "convert" character to number
     imul edi, 10 ; multiply "result so far" by ten
     add edi, ebp ; add in current digit
     jmp top ; until done
+done_m:
+    ret
 done:
-    ; Cierra el archivo
+    ;Cierra el archivo
     mov eax, 6
     mov ebx, [idArchivo]
     int 0x80
