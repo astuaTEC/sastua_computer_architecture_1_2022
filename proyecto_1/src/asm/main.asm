5b3762da927befe57dc508cd1fa7dde50507ece6
Saymon <sam.astua7@gmail.com>
2022-09-04 22:07:39 -0600

    Ya se calcula parte del algoritmo

--- proyecto_1/src/asm/main.asm
+++ proyecto_1/src/asm/main.asm
@@ -22,6 +22,8 @@ section .data
     archivo db "archivo.txt",0
     archivoDest db "destino.txt",0
 
+    strResult db 16 dup (0) ; string buffer to store results
+
 section .bss
 idArchivo resd 1
 contenido resb 50000 ; Almacenar n cantidad de bytes
@@ -40,6 +42,7 @@ h resd 1
 i resd 1
 j resd 1
 k resd 1
+l resd 1
 
 v1 resd 1
 v2 resd 1
@@ -143,9 +146,296 @@ getV4:
 
     mov [v4], edi
 
-    mov r14d, [v4]
+getA:
+    imul r10d, [v1], 2
+    mov rdx, 0
+    mov eax, r10d
+    mov ebx, 3
+
+    div ebx
+    mov r10d, eax
+
+    mov r11d, [v2]
+    mov rdx, 0
+    mov eax, r11d
+    mov ebx, 3
+
+    div ebx
+    mov r11d, eax
+
+    add r10d, r11d
+
+    mov [a], r10d
+
+getB:
+    mov r10d, [v1]
+    mov rdx, 0
+    mov eax, r10d
+    mov ebx, 3
+
+    div ebx
+    mov r10d, eax
+
+    imul r11d, [v2], 2
+    mov rdx, 0
+    mov eax, r11d
+    mov ebx, 3
+
+    div ebx
+    mov r11d, eax
+
+    add r10d, r11d
+
+    mov [b], r10d
+
+getC:
+    imul r10d, [v1], 2
+    mov rdx, 0
+    mov eax, r10d
+    mov ebx, 3
+
+    div ebx
+    mov r10d, eax
+
+    mov r11d, [v3]
+    mov rdx, 0
+    mov eax, r11d
+    mov ebx, 3
+
+    div ebx
+    mov r11d, eax
+
+    add r10d, r11d
+
+    mov [c], r10d
+
+getG:
+    mov r10d, [v1]
+    mov rdx, 0
+    mov eax, r10d
+    mov ebx, 3
+
+    div ebx
+    mov r10d, eax
+
+    imul r11d, [v3], 2
+    mov rdx, 0
+    mov eax, r11d
+    mov ebx, 3
+
+    div ebx
+    mov r11d, eax
+
+    add r10d, r11d
+
+    mov [g], r10d
+
+getK:
+    imul r10d, [v3], 2
+    mov rdx, 0
+    mov eax, r10d
+    mov ebx, 3
+
+    div ebx
+    mov r10d, eax
+
+    mov r11d, [v4]
+    mov rdx, 0
+    mov eax, r11d
+    mov ebx, 3
+
+    div ebx
+    mov r11d, eax
+
+    add r10d, r11d
+
+    mov [k], r10d
+
+getL:
+    mov r10d, [v3]
+    mov rdx, 0
+    mov eax, r10d
+    mov ebx, 3
+
+    div ebx
+    mov r10d, eax
+
+    imul r11d, [v4], 2
+    mov rdx, 0
+    mov eax, r11d
+    mov ebx, 3
+
+    div ebx
+    mov r11d, eax
+
+    add r10d, r11d
+
+    mov [l], r10d
+
+getF:
+    imul r10d, [v2], 2
+    mov rdx, 0
+    mov eax, r10d
+    mov ebx, 3
+
+    div ebx
+    mov r10d, eax
+
+    mov r11d, [v4]
+    mov rdx, 0
+    mov eax, r11d
+    mov ebx, 3
+
+    div ebx
+    mov r11d, eax
+
+    add r10d, r11d
+
+    mov [f], r10d
+
+getJ:
+    mov r10d, [v2]
+    mov rdx, 0
+    mov eax, r10d
+    mov ebx, 3
+
+    div ebx
+    mov r10d, eax
+
+    imul r11d, [v4], 2
+    mov rdx, 0
+    mov eax, r11d
+    mov ebx, 3
+
+    div ebx
+    mov r11d, eax
+
+    add r10d, r11d
+
+    mov [j], r10d
+
+getD:
+    imul r10d, [c], 2
+    mov rdx, 0
+    mov eax, r10d
+    mov ebx, 3
+
+    div ebx
+    mov r10d, eax
+
+    mov r11d, [f]
+    mov rdx, 0
+    mov eax, r11d
+    mov ebx, 3
+
+    div ebx
+    mov r11d, eax
+
+    add r10d, r11d
+
+    mov [d], r10d
+
+getE:
+    mov r10d, [c]
+    mov rdx, 0
+    mov eax, r10d
+    mov ebx, 3
+
+    div ebx
+    mov r10d, eax
+
+    imul r11d, [f], 2
+    mov rdx, 0
+    mov eax, r11d
+    mov ebx, 3
+
+    div ebx
+    mov r11d, eax
+
+    add r10d, r11d
+
+    mov [e], r10d
+
+getH:
+    imul r10d, [g], 2
+    mov rdx, 0
+    mov eax, r10d
+    mov ebx, 3
+
+    div ebx
+    mov r10d, eax
+
+    mov r11d, [k]
+    mov rdx, 0
+    mov eax, r11d
+    mov ebx, 3
+
+    div ebx
+    mov r11d, eax
+
+    add r10d, r11d
+
+    mov [h], r10d
+
+getI:
+    mov r10d, [g]
+    mov rdx, 0
+    mov eax, r10d
+    mov ebx, 3
+
+    div ebx
+    mov r10d, eax
+
+    imul r11d, [k], 2
+    mov rdx, 0
+    mov eax, r11d
+    mov ebx, 3
+
+    div ebx
+    mov r11d, eax
+
+    add r10d, r11d
+
+    mov [i], r10d
+
+    mov eax, [v1]    ; number to be converted
+    call int_to_string
 
     jmp done
+
+    imul r11, r8, 3
+    imul r12, r9, 3
+    mov [row2], r11d ; row2 = row*3
+    mov [col2], r12d ; col2 = col*3
+
+validateRowColumn:
+    cmp r8, 0
+    jne validateColumn1
+
+    cmp r9, 0
+    jne validateColumn2
+    ; arrayOut[row2][col2] = v1
+
+validateColumn2:
+    ; arrayOut[row2][col2+1] = a
+    ; arrayOut[row2][col2+2] = b
+    ; arrayOut[row2][col2+3] = v2
+
+validateColumn1:
+    cmp r9, 0
+    jne continue
+
+continue:
+; arrayOut[row2+1][col2+1] = d
+; arrayOut[row2+1][col2+2] = e
+; arrayOut[row2+1][col2+3] = f
+; arrayOut[row2+2][col2+1] = h
+; arrayOut[row2+2][col2+2] = i
+; arrayOut[row2+2][col2+3] = j
+
+; arrayOut[row2+3][col2+1] = k
+; arrayOut[row2+3][col2+2] = l
+; arrayOut[row2+3][col2+3] = v4
 
 error:
     mov eax, 4
@@ -171,6 +461,31 @@ top:
     jmp top ; until done
 done_m:
     ret
+
+int_to_string:
+    mov ecx, 10         ; divisor
+    xor bx, bx          ; count digits
+
+divide:
+    xor edx, edx        ; high part = 0
+    div ecx             ; eax = edx:eax/ecx, edx = remainder
+    push rdx             ; DL is a digit in range [0..9]
+    inc bx              ; count digits
+    test eax, eax       ; EAX is 0?
+    jnz divide          ; no, continue
+
+    ; POP digits from stack in reverse order
+    mov cx, bx          ; number of digits
+    lea esi, strResult   ; DS:SI points to string buffer
+
+next_digit:
+    pop ax
+    add ax, '0'         ; convert to ASCII
+    mov [esi], ax        ; write it to the buffer
+    inc si
+    loop next_digit
+    ret
+
 done:
     ;Cierra el archivo
     mov eax, 6
