713baf5cdbdc1620d3942f31574ad33030c48414
gaburolo <martinrolo22@estudiantec.cr>
2022-10-08 23:02:08 -0600

    Execute,Fetch,Controller,Decode provisionales

---
+++ proyecto_2/arquitectura/CPU.sv
@@ -0,0 +1,12 @@
+module CPU (input logic clk, reset, 
+	 output logic [31:0] PCF, 
+	 input logic [31:0] InstrF, 
+	 output logic MemWriteM, 
+	 output logic [31:0] ALUOutM, WriteDataM,
+	 input logic [31:0] ReadDataM);
+//Cables
+
+
+//Controller
+//Hazard
+endmodule
