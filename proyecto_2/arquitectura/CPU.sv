bf50805120c5e72f85f67aad08739ea25711ad23
Agustín V <joseagustinveneveg@gmail.com>
2022-10-19 05:13:38 -0600

    Ajustando frecuencia de clock (CPU)

--- proyecto_2/arquitectura/CPU.sv
+++ proyecto_2/arquitectura/CPU.sv
@@ -3,18 +3,25 @@ module CPU #(parameter WIDTH = 36, parameter REGNUM = 16,
 				parameter ADDRESSWIDTH = 4, parameter OPCODEWIDTH = 4, 
 				parameter INSTRUCTIONWIDTH = 24)
 				(input logic clock, reset, startIO, 
-				output logic outFlag, endFlag,
-				output logic [WIDTH-1:0] out);
-	logic clk_1Hz;
+				output logic outFlag, endFlag, clk_1Hz,
+				output logic [7:0] out);
+	//logic clk_1Hz;
+	logic [WIDTH-1:0] outaux;
+	logic flag = 0;
 	
 	divisorFrecuencia	divisorFrecuencia(clock, clk_1Hz); //la señal de 50Mhz se pasa a 1Hz
 
 	arqui #(WIDTH, REGNUM, ADDRESSWIDTH, OPCODEWIDTH, INSTRUCTIONWIDTH) 
-			arqui(clk_1Hz, reset, startIO, outFlag, out);
+			arqui(clk_1Hz, reset, startIO, outFlag, outaux);
+			
+	assign out = outaux[7:0];
 	
-	always_ff @(posedge clock) begin
-		if(outFlag === 1) endFlag <= 1;
-		else endFlag <= 0;
+	always_ff @(posedge clk_1Hz) begin
+		if(outaux === 500) begin
+			flag <= 1;
+			endFlag <= 1;
+		end
+		else if(flag === 0) endFlag <= 0;
 	end
 	
 endmodule 
