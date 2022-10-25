a701de4db6db4b2abedd1011102bf140b791b052
Agustín V <joseagustinveneveg@gmail.com>
2022-10-25 06:50:02 -0600

    Atualización de divisor de frecuencia

--- proyecto_2/arquitectura/divisorFrecuencia.sv
+++ proyecto_2/arquitectura/divisorFrecuencia.sv
@@ -9,7 +9,7 @@ reg[24:0] contador = 0; //Variable Contador equivale a 25 millones de estados.
 always @(posedge C_50Mhz)
     begin
         contador = contador + 1; //0.5 segundos LED encendido
-        if(contador == 2700)
+        if(contador == 25000)
         begin
             contador = 0;
             C_1Hz = ~C_1Hz; //0.5 segundos LED apagado
